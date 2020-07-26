653dc54a55720ab718ed18504aad6db71e86c019
svmuthesham <68824095+svmuthesham@users.noreply.github.com>
2020-07-27 01:50:34 +0530

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,9 +4,7 @@
     $message=$_POST['message'];
     $email_from='svmutheshamahmed@gmail.com';
     $email_subject="New form submission";
-    $email_body="username:$name.\n".
-                "user email:$visitor_email.\n".
-                "user message: $message.\n";
+    $email_body="username:$name.\n"."user email:$visitor_email.\n"."user message: $message.\n";
     $to="svmohteshamahmed@yahoo.in";
     $headers="from:$email_from\r\n";
     mail($to,$email_subject,$email_body,$headers);
