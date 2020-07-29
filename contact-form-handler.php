7297b706700d2585d26710845e25a6d894f22a74
svmuthesham <68824095+svmuthesham@users.noreply.github.com>
2020-07-29 08:58:35 +0530

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,11 @@
+<?php
+    $name=$_POST['name'];
+    $visitor_email=$_POST['email'];
+    $message=$_POST['message'];
+    $email_from='svmutheshamahmed@gmail.com';
+    $email_subject="New form submission";
+    $email_body="username: $name.\n" . "user email: $visitor_email.\n" . "user message: $message.\n";
+    $to="svmohteshamahmed@yahoo.in";
+    $headers="from:$email_from\r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("location:index.html");
